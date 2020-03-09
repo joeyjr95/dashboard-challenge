2a4edc1b364ca9915fa59459459d6e70633eab48
Joey Romo <joeyjr95@gmail.com>
2020-03-09 16:25:49 -0400

    top and bottom chart components started

---
+++ src/app/bottom-chart/bottom-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-bottom-chart',
+  templateUrl: './bottom-chart.component.html',
+  styleUrls: ['./bottom-chart.component.css']
+})
+export class BottomChartComponent {
+    
+}
---
+++ src/app/top-charts/top-charts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-top-charts',
+  templateUrl: './top-charts.component.html',
+  styleUrls: ['./top-charts.component.css']
+})
+export class TopChartsComponent {
+    
+}
